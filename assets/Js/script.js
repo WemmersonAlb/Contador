56f6019ac884c162116a82ecda83c987127cf1ec
WemmersonAlb <wemmerson.albuquerque@gmail.com>
2022-07-06 09:25:10 -0300

    Projeto de contador finalizado com todas as solicitãções do exercício

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+var incremento = window.document.getElementById("incremento");
+incremento.addEventListener("click", increment);
+var decremento = window.document.getElementById("decremento");
+decremento.addEventListener("click", decrement);
+
+
+function decrement() {    
+    if(currentNumber>=-2){
+        currentNumber = currentNumber-1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }    
+    if(currentNumber<0){
+        currentNumberWrapper.style.color = "red";
+    }
+    if(currentNumber==0){
+        currentNumberWrapper.style.color = "white";
+    }
+    
+}
+function increment() {    
+    if(currentNumber<=2){
+        currentNumber = currentNumber+1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }    
+    if(currentNumber>0){
+        currentNumberWrapper.style.color = "green";
+    }
+    if(currentNumber==0){
+        currentNumberWrapper.style.color = "white";
+    }
+}
